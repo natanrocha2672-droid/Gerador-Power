e45ec4b3c2be203391ad843cec3044badcdeb506
natanrocha2672-droid <natanrocha2672@gmail.com>
2026-06-08 16:40:23 -0300

    Corrige renderizacao dos modulos do curso

---
+++ app.js
@@ -0,0 +1,12 @@
+const base = window.COURSE_DATA || { title: "O Código da Agulha", modules: [] };
+
+const titles = [
+  "Boas-vindas e fundamentos do bordado",
+  "Materiais essenciais e organização do ateliê",
+  "Preparação do tecido e uso do bastidor",
+  "Transferência de risco e planejamento do desenho",
+  "Começar e terminar sem nós aparentes",
+  "Ponto alinhavo",
+  "Ponto atrás",
+  "Ponto haste",
+  "Ponto corrente",
